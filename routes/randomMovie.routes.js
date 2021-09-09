5dbc3a6d714da4f9380dba44d96b4bd4fe6a8e16
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2021-09-09 21:37:47 +0200

    random movies title generated on view

--- routes/randomMovie.routes.js
+++ routes/randomMovie.routes.js
@@ -3,15 +3,17 @@ const { isLoggedIn } = require("../middlewares/auth.config");
 const Movie = require("../models/Movie.model");
 const axios = require("axios");
 
-router.get("/random-movie", isLoggedIn, (req, res, next) => {
+//https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1` (Top rated route)
+//https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false (search movies route)
+
+https: router.get("/random-movie", isLoggedIn, (req, res, next) => {
   let index = Math.floor(Math.random() * 20);
   axios
     .get(
-      `
-https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`
     )
     .then((movieFromApi) => {
-      console.log("Here is your movie", movieFromApi.data.results[index]);
+      console.log("Here is your movie", movieFromApi.data);
 
       res.render("movies/random-movie.hbs", {
         movie: movieFromApi.data.results[index],

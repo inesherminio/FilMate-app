ce8a49cc0d9c13c6cd0306e594182373a5ee8982
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2021-09-11 13:10:59 +0200

    GEt route for profile added, destructured user not working

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
 const { isLoggedIn } = require("../middlewares/auth.config");
+const Movie = require("../models/Movie.model");
+const User = require("../models/User.model");
 
 // our routes here
 router.get("/", isLoggedIn, (req, res, next) => {
-  res.render("private/profile");
+  let movies;
+  Movie.find({ user: req.session.loggedInUser._id }, {}, { limit: 5 })
+    .then((moviesFromDB) => {
+      movies = moviesFromDB;
+      return User.find().populate("following");
+    })
+    .then((user) => {
+      //console.log(user.interests[0]);
+      const { interests, following } = user;
+      res.render("private/profile", {
+        movies,
+        interests,
+        following,
+      });
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;

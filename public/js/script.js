84b6ef77287c2990ff9e50a6b91a3533e058a95f
Leo Rawlinson <leo.m.rawlinson@gmail.com>
2021-09-25 11:14:41 +0200

    Stop the chat box from overflowing

--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,21 @@ editBtnArr.forEach((editBtn) => {
 $(document).ready(function () {
   $("#example-popover").popover();
 });
+
+//Attempt to clear the submit input for the chat
+/*
+function submitForm() {
+  let form = document.querySelector("#message-form");
+  form.submit();
+  form.reset();
+  return false;
+}
+
+let chatInput = document.querySelectorAll(".chat-text-input");
+let chatButton = document.querySelectorAll(".send-message-btn");
+
+chatButton.addEventListener("click", () => {
+  console.log("is this working?");
+  chatInput.value = "";
+});
+*/

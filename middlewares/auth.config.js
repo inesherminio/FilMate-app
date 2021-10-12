fc01eedb43d0d10f11e2cb910c7ec5c11ddeb3c5
Ines Herminio <inesherminio@gmail.com>
2021-10-12 20:53:51 +0100

    auth bugs solved

--- middlewares/auth.config.js
+++ middlewares/auth.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   isLoggedIn: (req, res, next) => {
     if (req.session.loggedInUser) {
-      req.app.locals.loggedInUser = req.session.loggedInUser;
-      req.app.locals.isLoggedIn = true;
-      req.app.locals.cookie = cookie;
       next();
     } else {
       res.redirect("/");
